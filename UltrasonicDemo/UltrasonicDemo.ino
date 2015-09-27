a4ac208efa63ce25ca65ed503f71b6f027c43535
connor <connormbrooks@gmail.com>
2015-09-27 16:52:37 -0500

    DANIEL here is a demo that should run to test out the ultrasonic sensors

---
+++ UltrasonicDemo/UltrasonicDemo.ino
@@ -0,0 +1,52 @@
+//Packages
+#include <Arduino.h>
+
+//Global Variable Declarations
+	
+	//PINS
+
+		//Input Pins
+			const int sonicInputPin = //PUT THE NUMBER OF THE PIN THAT IS CONNECTED TO THE SENSOR'S ECHO HERE
+
+		//Output Pins 
+			const int sonicTriggerPin = //PUT THE NUMBER OF THE PIN THAT IS CONNECTED TO THE SENSOR'S TRIGGER HERE
+
+	//GENERAL
+
+		long reading;
+
+void setup() {
+	Serial.begin(9600);
+	loadPins();
+}
+
+void loop() {
+	reading = getSonicDistance(sonicInputPin, sonicTriggerPin);
+	Serial.println(reading);	
+
+}
+
+
+
+void loadPins(){
+	//Input Pins
+	pinMode(sonicInputPin, INPUT);
+
+	//Output Pins
+	pinMode(sonicTriggerPin, OUTPUT);
+}
+
+long getSonicDistance(int echoPin, int triggerPin){
+	long distanceCM = 0;
+	digitalWrite(triggerPin,HIGH);
+	delayMicroseconds(300);
+	digitalWrite(triggerPin,LOW);
+	long duration = pulseIn(echoPin, HIGH,30000);
+	long distance = (duration*34029L)/2000000L;
+	distanceCM = distance;
+	if(distanceCM == 0){
+		distanceCM = 400;
+	}   
+	return (double)distanceCM; 
+}
+
